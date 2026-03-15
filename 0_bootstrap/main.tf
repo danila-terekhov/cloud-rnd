ad1ca6039a1baab4cb96d527486e06c5367337b1
poiza78 <poiza@list.ru>
2026-03-15 12:57:42 +0100

    Remove user creation from bootstrap manifest

--- 0_bootstrap/main.tf
+++ 0_bootstrap/main.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Identity: Сервисный пользователь
-resource "aws_iam_user" "tf_executor" {
-  name = "terraform-service-account"
-}
-
 # State Storage: Корзина для стейта
 resource "aws_s3_bucket" "tf_state" {
   bucket        = "danila-terekhov-cloud-rnd-tf-state-backend"
@@ -30,40 +25,4 @@ resource "aws_dynamodb_table" "tf_locks" {
     name = "LockID"
     type = "S"
   }
-}
-
-# Генерация политики (ждет создания S3 и DynamoDB для получения их ARN)
-data "aws_iam_policy_document" "tf_backend_policy" {
-  # Доступ к корзине
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:ListBucket"]
-    resources = [aws_s3_bucket.tf_state.arn]
-  }
-
-  # Доступ к объектам (самому стейту)
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
-    resources = ["${aws_s3_bucket.tf_state.arn}/*"]
-  }
-
-  # Доступ к таблице блокировок
-  statement {
-    effect    = "Allow"
-    actions   = ["dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem"]
-    resources = [aws_dynamodb_table.tf_locks.arn]
-  }
-}
-
-resource "aws_iam_policy" "tf_backend" {
-  name        = "TerraformBackendStrictAccess"
-  description = "Least privilege policy for Terraform state management"
-  policy      = data.aws_iam_policy_document.tf_backend_policy.json
-}
-
-# Аттач политики (ждет создания IAM User и IAM Policy)
-resource "aws_iam_user_policy_attachment" "tf_executor_attach" {
-  user       = aws_iam_user.tf_executor.name
-  policy_arn = aws_iam_policy.tf_backend.arn
 }
